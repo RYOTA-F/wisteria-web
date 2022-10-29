201efc742964e8e56e3b9a79ce5a4aa3f7c3a2fc
RYOTA-F <ry.fj0304@gmail.com>
2022-10-29 12:06:21 +0900

    :sparkles: Emotion

--- src/components/react/Button/index.tsx
+++ src/components/react/Button/index.tsx
@@ -1,11 +1,16 @@
 import type { FC } from "react";
+import styled from "@emotion/styled";
 
 const Button: FC = () => {
   const onClickButton = () => {
     console.log("test");
   };
 
-  return <button onClick={onClickButton}>テストテスト</button>;
+  return <Wrapper onClick={onClickButton}>テストテスト</Wrapper>;
 };
+
+const Wrapper = styled.button`
+  color: red;
+`;
 
 export default Button;
