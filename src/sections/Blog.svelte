cc28da9a18a23bcdbd55285f5dd132e9fef2611d
RYOTA-F <ry.fj0304@gmail.com>
2022-11-04 01:01:40 +0900

    :sparkles: Blog Section

---
+++ src/sections/Blog.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<section id="blog" class="blog">
+  Blog
+</section>
+
+<style>
+  .blog {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-top: 80px;
+    width: 100%;
+    height: 100vh;
+    background-color: #fff;
+    font-size: 70px;
+  }
+</style>
