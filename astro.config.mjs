bb989acfc28c1e0d4818e3391a2ca65eef598989
RYOTA-F <ry.fj0304@gmail.com>
2022-11-03 18:18:00 +0900

    :sparkles: Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: "server",
+  adapter: cloudflare()
 });
