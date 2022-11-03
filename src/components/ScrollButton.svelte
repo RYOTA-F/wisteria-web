cdf997119a55d9d75be3c442d3dfda22c6b3b180
RYOTA-F <ry.fj0304@gmail.com>
2022-11-04 01:22:51 +0900

    :sparkles: ScrollButton Component

---
+++ src/components/ScrollButton.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  const onClickScroll = () => window.scroll({top: 0, behavior: 'smooth'});
+</script>
+
+<button on:click={onClickScroll} class="scrollButton">
+  <div class="scrollButton__innnerArrow" />
+</button>
+
+<style>
+  .scrollButton {
+    position: fixed;
+    bottom: 50px;
+    right: 50px;
+    width: 50px;
+    height: 50px;
+    border: none;
+    border-radius: 50%;
+    color: #fff;
+    background-color: #709dd8;
+    cursor: pointer;
+  }
+  .scrollButton:hover {
+    background-color: #fff;
+    transition: 0.2s;
+  }
+  .scrollButton:hover > div {
+    border-color: #709dd8;
+  }
+
+  .scrollButton__innnerArrow {
+    position: absolute;
+    right: 50%;
+    top: 50%;
+    display: block;
+    width: 16px;
+    height: 16px;
+    margin-right: -8px;
+    margin-top: -5px;
+    content: "";
+    border-bottom: 4px solid #fff;
+    border-left: 4px solid #fff;
+    transform: rotate(135deg);
+  }
+</style>
