0427bd181d51df37acbdf450a55cabfe92e8bdd5
RYOTA-F <ry.fj0304@gmail.com>
2022-11-05 21:11:38 +0900

    :sparkles: Service Section

---
+++ src/sections/Service.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<section id="service" class="service">
+  Service
+</section>
+
+<style>
+  .service {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-top: 80px;
+    width: 100%;
+    height: 100vh;
+    background-color: #fff;
+    font-size: 70px;
+  }
+</style>
