b0ebab74d8cfa0ae4f75030f13e0271440defbd2
RYOTA-F <ry.fj0304@gmail.com>
2022-11-09 01:52:38 +0900

    :bug: Image Path

--- src/const/blog.ts
+++ src/const/blog.ts
@@ -4,32 +4,32 @@ const BLOG = {
   CONTENTS: [
     {
       TITLE: `【参加者50人Over！】オフライン勉強会・イベントレポート`,
-      IMAGE_SRC: '/dev_guil_event.png',
+      IMAGE_SRC: 'dev_guil_event.png',
       URL: 'https://kosotudev.com/devguil-2022-0625/',
     },
     {
       TITLE: `【Lambda × RDS Proxy】LambdaからProxy経由でRDSに接続してみる`,
-      IMAGE_SRC: '/rds.jpeg',
+      IMAGE_SRC: 'rds.jpeg',
       URL: 'https://kosotudev.com/rds-proxy/',
     },
     {
       TITLE: `【Route53 × WordPress】EC2からXserverへ移管するためのドメイン設定`,
-      IMAGE_SRC: '/route53.jpeg',
+      IMAGE_SRC: 'route53.jpeg',
       URL: 'https://kosotudev.com/route53-xserver/',
     },
     {
       TITLE: `【EC2】EC2にWordPressブログを構築してみた`,
-      IMAGE_SRC: '/ec2.jpeg',
+      IMAGE_SRC: 'ec2.jpeg',
       URL: 'https://kosotudev.com/ec2-server/',
     },
     {
       TITLE: `【file-saver】Reactでサーバーを介さずS3のオブジェクトをダウンロードしてみた`,
-      IMAGE_SRC: '/react_file_server.jpeg',
+      IMAGE_SRC: 'react_file_server.jpeg',
       URL: 'https://kosotudev.com/react-file-saver/',
     },
     {
       TITLE: `【Golang + GORM + MySQL】Go言語のORM「GORM」を使ってみた`,
-      IMAGE_SRC: '/golang.jpeg',
+      IMAGE_SRC: 'golang.jpeg',
       URL: 'https://kosotudev.com/go-gorm/',
     },
   ],
