db04d5f0d546e128663af8c7ede5a97ff3bc82c7
RYOTA-F <ry.fj0304@gmail.com>
2022-11-25 02:37:34 +0900

    update CareerSummary

--- src/const/career.ts
+++ src/const/career.ts
@@ -10,7 +10,17 @@ const ITEMS = [
 const CAREER = {
   TITLE_MAIN: '経歴',
   TITLE_SUB: 'Career',
-  SUMMARY: [`fdsafdsafdsa`, `fdsafdsafdsa`, `fdsafdsafdsa`],
+  NAME: 'RYOTA',
+  SUMMARY: [
+    [
+      '神奈川県在住のWeb系フリーランスエンジニア。',
+      'Webアプリケーションの開発やHPの制作等を中心に行なっています。',
+    ],
+    [
+      'フロントエンド開発/バックエンド開発からインフラの構築まで、幅広く手がけております。',
+      'Shopifyを使ったネットショップ開設も行っております。',
+    ],
+  ],
   ITEMS,
   SCROLL_IN_CLASS_NAME: 'scrollInItem_',
 } as const
--- src/sections/CareerSummary.svelte
+++ src/sections/CareerSummary.svelte
@@ -1,27 +1,43 @@
 <script lang="ts">
   import CAREER from '../const/career'
+  import H3 from '../components/H3.svelte';
 </script>
 
 <section id="career_summary" class="careerSummary">
-  {#each CAREER.SUMMARY as item }
-    <p class="description">{item}</p>
-  {/each}
+  <H3>{CAREER.NAME}</H3>
+
+  <div class="descriptionContainer">
+    {#each CAREER.SUMMARY as items }
+      <ul class="listContainer">
+        {#each items as item }
+          <li class="description">{item}</li>
+        {/each}
+      </ul>
+    {/each}
+  </div>
 </section>
 
 <style>
   .careerSummary {
     width: 100%;
-    padding: 120px 10%;
-    text-align: center;
+    padding: 60px 20%;
     background-color: #f1f1f1;
   }
 
-  .description {
-    font-size: 16px;
-    white-space: pre-wrap;
+  .descriptionContainer {
+    padding: 30px;
   }
 
+  .listContainer:not(:first-child) {
+    margin-top: 20px;
+  }
+
+  :not(:first-child)
+
+  .description {
+    font-size: 16px;
+  }
   .description:not(:first-child) {
-    margin-top: 10px;
+    margin-top: 8px;
   }
 </style>
