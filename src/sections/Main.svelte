13ddef133d69a82ee7977232e38c782aee2f300c
RYOTA-F <ry.fj0304@gmail.com>
2022-11-26 02:26:01 +0900

    update Main

--- src/sections/Main.svelte
+++ src/sections/Main.svelte
@@ -1,53 +1,125 @@
 <script lang="ts" type="module">
+	import { onMount } from 'svelte';
   import MAIN from '../const/main'
 
-  import Particles from "svelte-particles";
-  import { loadFull } from "tsparticles";
+  import { gsap } from 'gsap'
 
-  let particlesConfig = {
-      particles: {
-          color: {
-              value: "#000",
-          },
-          links: {
-              enable: true,
-              color: "#000",
-          },
-          move: {
-              enable: true,
-          },
+  onMount(() => {
+
+    gsap.to(".background-color", {
+      duration: 1, //アニメーションの時間の設定
+      opacity: 1, //表示状態の指定
+    });
+
+    //.textを1.5秒かけ出現させ1.5秒かけ逆再生させ初期状態に戻す
+    gsap.to(".text", {
+      delay: 1, //アニメーションの初期遅延（アニメーションが開始されるまでの指定）
+      duration: 1, //アニメーションの時間の設定
+      opacity: 1, //表示状態の指定
+      y: "0px", //cssでY軸-20pxから完了時0pxへの指定
+      yoyo: true, //trueにすることで上記で指定したY軸の動きの逆再生
+      repeat: 1, //yoyoのリピート回数
+      //同じclass名のバリデーションの設定
+      stagger: {
+        each: 0.5, //ディレイ時間
       },
-  };
+    });
 
-  const onParticlesLoaded = (event: any) => {
-      // const particlesContainer = event.detail.particles;
-  };
-
-  const particlesInit = async (engine: any) => {
-      await loadFull(engine);
-  };
+    //初期遅延3.5秒かけてから.titleを表示
+    gsap.to(".title span", {
+      delay: 3.5, //アニメーションの初期遅延（アニメーションが開始されるまでの指定）
+      duration: 1.5, //アニメーションの時間の設定
+      opacity: 1, //表示状態の指定
+      //同じclass名のバリデーションの設定
+      stagger: {
+        each: 0.2, //ディレイ時間
+        from: "start", //出現方法の指定
+      },
+    });
+  })
 </script>
 
 <section id="main" class="main">
-  <Particles
-    id="tsparticles"
-    options={particlesConfig}
-    on:particlesLoaded={onParticlesLoaded}
-    particlesInit={particlesInit}
-  />
+  <!-- <p class="typewriter__text typewriter__animation" >
+    {MAIN.WELCOME}
+    <span class="siteName">{MAIN.TITLE}</span>
+    {MAIN.EXCLAMATION}
+  </p> -->
+  <h1 class="text">CREATE PLAY ENJOY</h1>
+    <p class="text">作って、遊んで、楽しむ</p>
+    <h1 class="title">
+      <span>T</span>
+      <span>O</span>
+      <span>K</span>
+      <span>O</span>
+      <span>S</span>
+      <span>&nbsp</span>
+      <span>i</span>
+      <span>n</span>
+      <span>c</span>
+      <span>.</span>
+    </h1>
 </section>
 
 <style>
   .main {
-    /* display: flex;
+    display: flex;
     align-items: center;
-    justify-content: center; */
+    justify-content: center;
     width: 100%;
-    max-height: 100vh;
+    height: 100vh;
     padding-top: 80px;
     background-color: #709dd8;
     color: #fff;
   }
+
+  .text-area {
+  width: 100%;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+h1 {
+  font-size: 50px;
+  margin: 0;
+  text-align: center;
+  letter-spacing: 1px;
+}
+
+p {
+  font-size: 16px;
+  margin: 0;
+  text-align: center;
+}
+
+.background-color {
+  width: 100%;
+  height: 100vh;
+  background-color: black;
+  opacity: 0;
+  position: relative;
+}
+
+.text {
+  opacity: 0;
+  transform: translateY(20px);
+  color: #ffffff;
+}
+
+.title {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.title span {
+  opacity: 0;
+  color: #ffffff;
+}
 
   .typewriter__text{
     position: relative;
