c8c30a3a568bb73547219768bedf5682f91e7ce9
RYOTA-F <ry.fj0304@gmail.com>
2022-12-12 21:07:43 +0900

    add Document Section

--- src/sections/Document.svelte
+++ src/sections/Document.svelte
@@ -10,12 +10,13 @@
   <H3>{DOCUMENT.TITLE_SUB}</H3>
   <H2>{DOCUMENT.TITLE_MAIN}</H2>
   <ul class="documentContainer">
-    <li class="documentItem">
-      <iframe class="speakerdeck-iframe" frameborder="0" src="https://speakerdeck.com/player/f889fcb4c9c14d11ba70c452d9b48a92" title="Web技術 で デスクトップアプリ !!? 〜 Electron のすすめ〜" allowfullscreen={true} style="border: 0px; background: padding-box padding-box rgba(0, 0, 0, 0.1); margin: 0px; padding: 0px; border-radius: 6px; box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 40px; width: 100%; height: 100%;" data-ratio="1.78343949044586"></iframe>
-    </li>
-    <li class="documentItem">
-      <iframe class="speakerdeck-iframe" frameborder="0" src="https://speakerdeck.com/player/42112e4ab6914128b284f6815b1a35e9" title="ブロックチェーンを活用したスキル証明技術.pdf" allowfullscreen={true} style="border: 0px; background: padding-box padding-box rgba(0, 0, 0, 0.1); margin: 0px; padding: 0px; border-radius: 6px; box-shadow: rgba(0, 0, 0, 0.2) 0px 5px 40px; width: 100%; height: 100%;" data-ratio="1.7777777777777777"></iframe>
-    </li>
+    {#each DOCUMENT.ITEMS  as ITEM }
+      <li class="documentItem">
+        <a href={ITEM.URL} target="__brank">
+          <img src={`images/documents/${ITEM.SRC}`} alt={ITEM.ALT} class="documentImage" />
+        </a>
+      </li>
+    {/each}
   </ul>
 </section>
 
@@ -35,8 +36,34 @@
     margin: 0 auto;
     width: 60vw; /* 1.78 : 1 */
     height: 34vw;
+    border-radius: 10px;
+    overflow: hidden;
   }
   .documentItem:not(:first-child) {
     margin-top: 60px;
+  }
+
+  .documentImage {
+    border-radius: 10px;
+    transition: 0.3s all;
+  }
+  .documentImage:hover {
+    transform: scale(1.05, 1.05);
+    transition: 0.3s all;
+    opacity: 0.7;
+  }
+
+  @media screen and (max-width: 600px) {
+    .document {
+      min-height: 0;
+      padding: 40px 0;
+    }
+    .documentItem {
+      width: 86vw; /* 1.78 : 1 */
+      height: 48vw;
+    }
+    .documentItem:not(:first-child) {
+      margin-top: 30px;
+    }
   }
 </style>
